9cf85bc52c2199f1ce903836317bd1cbd8e218e6
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2025-03-09 13:54:50 +0100

    12

--- smartedu/settings.py
+++ smartedu/settings.py
@@ -129,12 +129,12 @@ USE_TZ = True
 # .\env\Scripts\activate
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')] 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
+
+
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
