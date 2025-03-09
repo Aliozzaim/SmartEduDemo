3208e3af32f966fe5f83668175f3d7a0f093f4d9
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2025-03-09 13:45:07 +0100

    dsa

--- smartedu/settings.py
+++ smartedu/settings.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -131,6 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
